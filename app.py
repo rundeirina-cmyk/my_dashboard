a3c7fa21062f29eb264e34bf78e9dca252b62464
rundeirina-cmyk <runde.irina@gmail.com>
2026-08-31 17:16:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import streamlit.web.cli as stcli
 import sys
 
 if __name__ == '__main__':
-    sys.argv = ["streamlit", "run", "dashboard.py", "--server.port", "80", "--server.address", "0.0.0.0"]
+    sys.argv = ["streamlit", "run", "dashboard.py", "--server.port", "8080", "--server.address", "0.0.0.0"]
     sys.exit(stcli.main())

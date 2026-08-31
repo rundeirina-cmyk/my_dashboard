afd853859b06886fffd89ad076e03b6e89643007
rundeirina-cmyk <runde.irina@gmail.com>
2026-08-31 19:19:58 +0300

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-import streamlit.web.cli as stcli
+import subprocess
 import sys
 
 if __name__ == '__main__':
-    sys.argv = ["streamlit", "run", "dashboard.py", "--server.port", "8080", "--server.address", "0.0.0.0"]
-    sys.exit(stcli.main())
+    subprocess.run([
+        sys.executable,
+        "-m", "streamlit",
+        "run", "dashboard.py",
+        "--server.port", "8080",
+        "--server.address", "0.0.0.0"
+    ])
